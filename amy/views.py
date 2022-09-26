c428d22f01c6dfd7a5f2ee58dca1dbbec46e0eb0
Piotr Sapiezynski <p.sapiezynski@northeastern.edu>
2022-09-26 09:35:45 -0400

    syncing heroku

--- amy/views.py
+++ amy/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from .models import Attendee
+from django.http import JsonResponse
 
+from .models import Attendee
+from django.views.decorators.csrf import csrf_exempt
+
+import pdb
 # Create your views here.
 
 def index(request):
@@ -12,5 +16,22 @@ def all_attendees(request):
 		"attendees": attendees
 		})
 
+@csrf_exempt
 def thanks(request):
+	print(request)
+	try:
+		if request.method == "POST":
+			att = Attendee(title=request.POST.get('name', 'no'),
+				email = request.POST.get('email', 'no'),
+				size = request.POST.get('count', 'no'),
+				rsvp = request.POST.get('att', 'no'),
+				consent = request.POST.get('covid', 'no'))
+			print(att)
+			att.save()
+			return JsonResponse({'type': 'success'})
+	except Exception as e:
+		return JsonResponse({'type': 'error', 'reason': str(e)})
+			
+		
+
 	return render(request, "amy/thanks.html")
